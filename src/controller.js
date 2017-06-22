23befcafe80b66f69a3b46aa9d83a68b37362b47
Dmitriy <dima.chebotaru@gmail.com>
2017-06-22 20:28:50 +0300

    Final

--- src/controller.js
+++ src/controller.js
@@ -1,18 +1,42 @@
 class Controller {
   constructor(model, view) {
-    this.model = model;
-    this.view = view;
+  this.model = model;
+  this.view = view;
+    
+  view.on('add', this.addTodo.bind(this));
+  view.on('toggle', this.toggleTodo.bind(this));
+  view.on('edit', this.editTodo.bind(this));
+  view.on('remove', this.removeTodo.bind(this));
 
-    view.on('add', this.addTodo.bind(this));
+  view.show(model.items);
   }
 
   addTodo(title) {
-    const todo = this.model.addItem({
+    const item = this.model.addItem({
       id: Date.now(),
       title,
       completed: false
     });
-    
-    this.view.addItem(todo);
+
+    this.view.addItem(item);
+  }
+
+  toggleTodo({ id, completed }) {
+    const item = this.model.updateItem(id, { completed });
+
+    this.view.toggleItem(item);
+  }
+
+  editTodo({ id, title }) {
+    const item = this.model.updateItem(id, { title });
+
+    this.view.editItem(item);
+  }
+
+  removeTodo(id) {
+    this.model.removeItem(id);
+    this.view.removeItem(id);
   }
 }
+
+export default Controller;
